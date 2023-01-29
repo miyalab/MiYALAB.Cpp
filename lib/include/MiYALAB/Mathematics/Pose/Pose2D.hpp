e6c44968f87ce84eac8eb3bfb2cfa1467e5e9a5b
miyalab <miyalab.workspace@gmail.com>
2023-01-29 15:48:36 +0900

    update

--- lib/include/MiYALAB/Mathematics/Pose/Pose2D.hpp
+++ lib/include/MiYALAB/Mathematics/Pose/Pose2D.hpp
@@ -2,7 +2,7 @@
 /*
  * MIT License
  * 
- * Copyright (c) 2022 MiYA LAB(K.Miyauchi)
+ * Copyright (c) 2022-2023 MiYA LAB(K.Miyauchi)
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
