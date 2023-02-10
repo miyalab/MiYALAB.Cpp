ef8b654c86b793257fd079104e2dd418caf24b6c
miyalab <miyalab.workspace@gmail.com>
2023-02-10 15:02:04 +0900

    update

---
+++ lib/include/MiYALAB/Robotics/LaserScan/LaserScan.hpp
@@ -0,0 +1,64 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2022-2023 MiYA LAB(K.Miyauchi)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+
+#ifndef __MIYALAB_CPP_ROBOTICS_LASER_SCAN_LASER_SCAN_HPP__
+#define __MIYALAB_CPP_ROBOTICS_LASER_SCAN_LASER_SCAN_HPP__
+
+//-----------------------------
+// include
+//-----------------------------
+#include <vector>
+
+//-----------------------------
+// Namespace & using
+//-----------------------------
+
+//-----------------------------
+// Struct
+//-----------------------------
+namespace MiYALAB {
+namespace Robotics{
+struct LaserScan{
+public:
+    LaserScan();
+    virtual ~LaserScan();
+
+    float angleMin;
+    float angleMax;
+    float angleIncrement;
+    float timeIncrement;
+    float scanTime;
+    float rangeMin;
+    float rangeMax;
+    std::vector<float> ranges;
+    std::vector<float> intensities;
+};
+}
+}
+
+#endif // __MIYALAB_CPP_ROBOTICS_LASER_SCAN_LASER_SCAN_HPP__
+
+//-----------------------------------------------------------------------------------
+// end of file
+//-----------------------------------------------------------------------------------
