7194d42bd9393815930e331961087cb5fe58d364
miyalab <miyalab.workspace@gmail.com>
2023-02-11 19:13:41 +0900

    update

---
+++ lib/include/MiYALAB/Robotics/PointCloud/PointCloud.hpp
@@ -0,0 +1,63 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2022-2023 MiYA LAB(K.Miyauchi)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+
+#ifndef __MIYALAB_CPP_ROBOTICS_POINT_CLOUD_HPP__
+#define __MIYALAB_CPP_ROBOTICS_POINT_CLOUD_HPP__
+
+//-----------------------------
+// include
+//-----------------------------
+#include <vector>
+
+#include "MiYALAB/Mathematics/Point/Point3D.hpp"
+
+//-----------------------------
+// Namespace & using
+//-----------------------------
+
+//-----------------------------
+// Struct
+//-----------------------------
+namespace MiYALAB {
+namespace Robotics{
+struct PointCloud{
+public:
+    PointCloud();
+    virtual ~PointCloud();
+
+    std::vector<Mathematics::Point32f> points;
+    struct Channel{
+        std::string name;
+        std::vector<float> values;
+    };
+    std::vector<Channel> channels;
+};
+}
+}
+
+#endif // __MIYALAB_CPP_ROBOTICS_POINT_CLOUD_HPP__
+
+//-----------------------------------------------------------------------------------
+// end of file
+//-----------------------------------------------------------------------------------
